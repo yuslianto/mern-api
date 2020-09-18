bb076e1968f9ebb9681ea47c4218fde4cd81ea9f
yuslianto <yuslianto56@gmail.com>
2020-09-18 07:06:02 +0700

    membuat dan memahami basic router, requaet dan response di nodeJS

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 
-app.use(() => {
-    //console.log('hello server ...');
-    console.log('hello lagi server node js');
-})
+router.use('/products', (req, res, next) => {
+    res.json({name: "Yuslianto", email: "yuslianto@gmail.com"});
+    next();
+});
+
+router.use('/price', (req, res, next) => {
+    res.json({price: 300000});
+    next();
+});
+
+router.delete('/customers', (req, res, next) => {
+    res.json({title: "Customers"});
+    next();
+});
+
+app.use('/', router);
 
 app.listen(4000);
